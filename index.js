823b6569e13e92b6c6c1ba7b16ebe1b91ccec8c3
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-08-07 21:14:54 +0200

    début du javascript

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 // api key = 0c8b8326153eec2588a0cb95677df6f2
 function fetchData() {
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=london&APPID=0c8b8326153eec2588a0cb95677df6f2"
+    "https://api.openweathermap.org/data/2.5/weather?q=Paris&APPID=0c8b8326153eec2588a0cb95677df6f2"
   )
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
 
 fetchData();
+
+let country = "";
+
+const inputSearch = document.querySelector("#search");
+console.log(inputSearch);
+
+// On recupère ce qu'on tape dans l'input
+inputSearch.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  country = e.target.value;
+  console.log(country);
+});
+
+const searchBtn = document.querySelector("#search-btn");
+searchBtn.addEventListener("click", () => {
+  alert("ok");
+});
