9b4109375cf291d212ac0b1dafc6e2e336139812
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-08-08 18:40:13 +0200

    mise en place de la recherche par nom et par code postal

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // api key = bQUiDye9ApfNGmVKSKzBut2EaW6McX9W
 // let arrayCountryMain;
+//  postal "http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W&q=93300"
+
 import { dataIcon } from "./dataIcon.js";
 
 console.log(dataIcon);
@@ -7,27 +9,35 @@ let mainDetails = [];
 let weatherDetails = [];
 let keyCountrie;
 async function fetchData() {
-  await fetch(
-    "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W&q=london"
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      //   displayData(data[0]);
-      mainDetails = data[0];
-      keyCountrie = mainDetails.Key;
-      //   keyCountrie = key;
-      console.log(mainDetails);
-    });
-
-  if (mainDetails) {
+  try {
     await fetch(
-      `http://dataservice.accuweather.com/currentconditions/v1/${keyCountrie}?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W`
+      "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W&q=london"
     )
       .then((res) => res.json())
       .then((data) => {
-        weatherDetails = data[0];
-        console.log(weatherDetails);
+        //   displayData(data[0]);
+        mainDetails = data[0];
+        keyCountrie = mainDetails.Key;
+        //   keyCountrie = key;
+        console.log(mainDetails);
       });
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (mainDetails) {
+    try {
+      await fetch(
+        `http://dataservice.accuweather.com/currentconditions/v1/${keyCountrie}?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          weatherDetails = data[0];
+          console.log(weatherDetails);
+        });
+    } catch (err) {
+      console.log(err);
+    }
   }
   //   fusionne les tableau
   //   let arrayCountryMain = [...mainDetails, ...weatherDetails];
@@ -39,6 +49,7 @@ fetchData();
 
 let country = "";
 let arrayCountry = [];
+let postalCode;
 
 const inputSearch = document.querySelector("#search");
 console.log(inputSearch);
@@ -46,7 +57,21 @@ console.log(inputSearch);
 // On recupère ce qu'on tape dans l'input
 inputSearch.addEventListener("input", (e) => {
   console.log(e.target.value);
-  country = e.target.value;
+
+  let inputValue = e.target.value;
+  // on check si l'utilisateur entre des lettre
+  let checkString = /^[a-zA-Z ]+$/.test(inputValue);
+  // si c'est vrai alors ça sera une recherche avec le nom de la ville
+  if (checkString == true) {
+    country = e.target.value;
+    console.log("c'est bien un string");
+  } else if (checkString == false) {
+    postalCode = e.target.value;
+    console.log("c'est bien un number");
+  } else {
+    console.log("il y a une erreur");
+    return false;
+  }
 });
 
 function displayData(array, details) {
@@ -86,18 +111,44 @@ let keyCountrieSearch;
 
 searchBtn.addEventListener("click", async () => {
   console.log(country);
-  await fetch(
-    ` http://dataservice.accuweather.com/locations/v1/cities/search?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W&q=${country}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      //   displayData(data[0]);
-      mainDetailsSearch = data[0];
-      keyCountrieSearch = mainDetailsSearch.Key;
-      //   keyCountrie = key;
-      console.log(mainDetailsSearch);
-    });
+  if (country) {
+    await fetch(
+      ` http://dataservice.accuweather.com/locations/v1/cities/search?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W&q=${country}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        //   displayData(data[0]);
+        mainDetailsSearch = data[0];
+        keyCountrieSearch = mainDetailsSearch.Key;
+        //   keyCountrie = key;
+        console.log(mainDetailsSearch);
+      });
 
+    if (mainDetailsSearch) {
+      await fetch(
+        `http://dataservice.accuweather.com/currentconditions/v1/${keyCountrieSearch}?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          weatherDetailsSearch = data[0];
+          console.log(weatherDetailsSearch);
+        });
+    }
+
+    displayData(mainDetailsSearch, weatherDetailsSearch);
+  } else if (postalCode) {
+    await fetch(
+      ` http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W&q=${postalCode}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        //   displayData(data[0]);
+        mainDetailsSearch = data[0];
+        keyCountrieSearch = mainDetailsSearch.Key;
+        //   keyCountrie = key;
+        console.log(mainDetailsSearch);
+      });
+  }
   if (mainDetailsSearch) {
     await fetch(
       `http://dataservice.accuweather.com/currentconditions/v1/${keyCountrieSearch}?apikey=bQUiDye9ApfNGmVKSKzBut2EaW6McX9W`
@@ -108,8 +159,6 @@ searchBtn.addEventListener("click", async () => {
         console.log(weatherDetailsSearch);
       });
   }
-  //   fusionne les tableau
-  //   let arrayCountryMain = [...mainDetails, ...weatherDetailsSearch];
-  //   console.log(arrayCountryMain);
+
   displayData(mainDetailsSearch, weatherDetailsSearch);
 });
